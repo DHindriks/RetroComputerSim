320371bd2f5206ac0773728c8343765f2f07c265
GabiElRumeno <danescugabriel2603@gmail.com>
2024-06-06 10:11:52 +0200

    added a chat bubble image and test around the script

--- Assets/Prefabs/chatBubble.cs
+++ Assets/Prefabs/chatBubble.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class chatBubble : MonoBehaviour
 {
     private SpriteRenderer backgroundSpriteRenderer;
     private TextMeshPro textMeshPro;
     private void Awake(){
-        backgroundSpriteRenderer = transform.find("Background").GetComponent<SpriteRenderer>();
+        backgroundSpriteRenderer = transform.Find("chatBubbleBackground").GetComponent<SpriteRenderer>();
+        textMeshPro = transform.Find("Text").GetComponent<TextMeshPro>();
+    }
+
+    private void Start(){
+    Setup("This should go on first line \n This on the second");
+    }
+    
+    private void Setup(string text){
+        textMeshPro.SetText(text);
+        textMeshPro.ForceMeshUpdate();
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+        Vector2 padding = new Vector2 (6f, 2f);
+        backgroundSpriteRenderer.size = textSize + padding;
     }
 }
